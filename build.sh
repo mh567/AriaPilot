2046d30ada985ceda55c232dc3eb2eb5b35ffd40
Lu Harris <harris@LudeMacBook-Air-4.local>
2026-06-15 20:57:12 +0800

    chore: bump version to 1.5.3

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -e
 
 APP_NAME="AriaPilot"
-APP_VERSION="1.5.2"
-BUILD_NUMBER="152"
+APP_VERSION="1.5.3"
+BUILD_NUMBER="153"
 BUILD_DIR=".build/release"
 APP_BUNDLE="$APP_NAME.app"
 CONTENTS="$APP_BUNDLE/Contents"
